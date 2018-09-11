17b6e7b4b96bb02f5a72d735600f2118cccd24ab
mip-develop1 <michishita@mips.co.jp>
2018-09-11 20:24:44 +0900

    merge input name
    merge input name

--- Sample_Bluetooth/DeviceSelectViewController.swift
+++ Sample_Bluetooth/DeviceSelectViewController.swift
@@ -11,101 +11,172 @@ import UIKit
 import CoreBluetooth
 import RealmSwift
 
-class DeviceSelectViewController: UIViewController {
-    
-    var tableView: UITableView!
-    var serviceUuids = [String]()
-    var services = [CBService]()
-    var targetPeripheral: CBPeripheral!
+
+class DeviceSelectViewController:  UIViewController,CBCentralManagerDelegate,CBPeripheralDelegate,UITextFieldDelegate {
     var centralManager: CBCentralManager!
-    var targetService: CBService!
-    var readValues = [Data]()
+    var peripheral: CBPeripheral!
+    var charactaristic: CBCharacteristic!
+    let serviceUUID:CBUUID = CBUUID(string: "135E7F5F-D98B-413C-A0BE-CAC8E3F53280")
+    let charactaristicUUID_name:CBUUID = CBUUID(string: "810E2E4E-2E03-46F5-91FB-A238A8E127B5")
+    var devicename:String?
     
-    // サービスが見つかった時、仮の目的serviceUUID
-    let targetServiceUUID : String = "0x180D"
+    var isdone:Bool = false
+    
+    
+    @IBOutlet var name: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let barH:CGFloat = UIApplication.shared.statusBarFrame.size.height
-        let dispW:CGFloat = self.view.frame.width
-        let dispH:CGFloat = self.view.frame.height
-        
-        tableView = UITableView(frame: CGRect(x: 0, y: barH, width: dispW, height: dispH - barH))
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
-        tableView.dataSource = self
-        tableView.delegate = self
-        self.view.addSubview(tableView)
-        
+        self.centralManager = CBCentralManager(delegate: self, queue: nil)
+        print("start")
+        //self.centralManager.scanForPeripherals(withServices: [self.serviceUUID], options: nil)
     }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        self.centralManager.scanForPeripherals(withServices: [self.serviceUUID], options: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-}
-extension DeviceSelectViewController: CBPeripheralDelegate {
+    
+    @IBAction func scanbutton(_ sender: Any) {
+        self.centralManager.scanForPeripherals(withServices: [self.serviceUUID], options: nil)
+    }
+    
+    // 追加
+    // ここからが前のページからperipheral,centralmanagerを引き継ぐ関数
     func setPeripheral(target: CBPeripheral) {
-        self.targetPeripheral = target
+        self.peripheral = target
     }
     func setCentralManager(manager: CBCentralManager) {
         self.centralManager = manager
     }
+    //前のページでservice検索を行うための関数
     func searchService() {
         print("searchService")
-        self.targetPeripheral.delegate = self
+        self.peripheral.delegate = self
         // service の探索条件(serviceUUID)をどう設定するか？複数 or 一つ(let targetServiceUUID)？
-        self.targetPeripheral.discoverServices(nil)
+        self.peripheral.discoverServices(nil)
+    }
+    //ここまで
+    
+    //画面遷移時に呼ばれる
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if self.peripheral != nil{
+            self.centralManager.cancelPeripheralConnection(self.peripheral)
+        }
     }
     
+    //centralManagerが更新した時に呼ばれる
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        print("state: \(central.state)")
+    }
+    
+    //ペリフェラルを検出した時に呼び出される
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        print("発見したBLEデバイス:\(peripheral.identifier)")
+        let uuid = peripheral.identifier.uuidString
+        //if uuid == "090C2471-2BF2-6448-5C28-BA3C2C01645B"{//ミップスタッフのiphone(2) DBに登録されている装置のUUIDに変更
+        if uuid == "91B7541E-A6DC-2484-2DB4-57CF8F0A114E"{//テスト用iPad
+            self.peripheral = peripheral
+            self.centralManager.connect(self.peripheral, options: nil)
+        }
+    }
+    
+    //ペリフェラとのコネクトに成功した時に呼び出される
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print("接続成功")
+        peripheral.delegate = self
+        peripheral.discoverServices([self.serviceUUID])
+    }
+    //ペリフェラルのコネクトに失敗した時に呼び出される
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        print("接続失敗")
+    }
+    //ペリフェラルのサービスを検出した時に呼び出される
     func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
-        if error != nil {
-            print("discoverServicesError: \(error.debugDescription)")
+        print("サービス検出")
+        if error != nil{
+            print(error.debugDescription)
             return
         }
-
-        // 発見した service 全てを格納(暗に探索条件を複数serviceで想定)
-        for service in peripheral.services! {
-            serviceUuids.append(service.uuid.uuidString)
-            services.append(service)
-            if service.uuid.uuidString == targetServiceUUID {
-                targetService = service
+        
+        //キャラクタリスティック 検索
+        peripheral.discoverCharacteristics(nil, for: (peripheral.services?.first)!)
+    }
+    
+    //キャラクタリスティック 検出じに呼び出される
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        let characteristics = service.characteristics
+        if error != nil{
+            print(error.debugDescription)
+            return
+        }
+        print("\(String(describing: service.characteristics?.count))個のキャラクタリスティック を検知")
+        
+        for obj in characteristics!{
+            if let characteristic = obj as? CBCharacteristic{
+                print(characteristic.uuid)
+                switch characteristic.uuid{
+                case self.charactaristicUUID_name:
+                    if self.isdone == false{
+                        print("nameのキャラクタリスティック を検出(read)")
+                        peripheral.readValue(for: characteristic)
+                    }else{
+                        print("nameのキャラクタリスティック を検出(write)")
+                        let namedata: Data? = name.text?.data(using: .utf8)!
+                        self.peripheral.writeValue(namedata!, for: charactaristic, type: CBCharacteristicWriteType.withoutResponse)
+                    }
+                default:
+                    print("指定外のキャラクタリスティック を検出")
+                }
+                self.charactaristic = characteristic
             }
         }
-        // 目的service が見つからなかった場合
-        if targetService == nil {
-            
-        }
-        self.targetPeripheral.discoverCharacteristics(nil, for: self.targetService)
+        
     }
-    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
-        if error != nil {
-            print("discoverCharacteristicError: \(error.debugDescription)")
-            return
-        }
-    }
+    
+    //readリクエストにレスポンスが返ってきたら呼び出される
     func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
-        if error != nil {
-            print("updateValue characteristic: \(error.debugDescription)")
-            return
+        switch characteristic.uuid{
+        case self.charactaristicUUID_name:
+            self.devicename = String(data: characteristic.value!, encoding: .utf8)
+            print(self.devicename)
+            //readしたら接続をきる
+            self.name.text = self.devicename
+            self.centralManager.cancelPeripheralConnection(self.peripheral)
+            print("切断")
+        default:
+            print("指定外のキャラクタリスティック のレスポンスを検出")
         }
     }
-}
-extension DeviceSelectViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
-        print("ServiceUuid: \(serviceUuids[indexPath.row])")
+    
+    //wirteリクエストにレスポンスが返ってきたら呼び出される
+    func peripheral(_ peripheral: CBPeripheral, didWriteValueFor characteristic: CBCharacteristic, error: Error?) {
+        if let error = error{
+            print("Write失敗...error:\(error)")
+            return
+        }
+        
+        print("Write成功")
+        //writeしたら接続をきる
+        self.centralManager.cancelPeripheralConnection(self.peripheral)
+        self.isdone = false
+        print("切断")
     }
-}
-extension DeviceSelectViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return serviceUuids.count
+    
+    //完了ボタン押す
+    @IBAction func done(_ sender: Any) {
+        self.isdone = true
+        self.centralManager.connect(self.peripheral, options: nil)
     }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
-        let cell = UITableViewCell(style: UITableViewCellStyle.subtitle, reuseIdentifier: "SVcell")
-        cell.textLabel!.sizeToFit()
-        cell.textLabel!.textColor = UIColor.red
-        cell.textLabel!.text = "\(serviceUuids[indexPath.row])"
-        cell.textLabel!.font = UIFont.systemFont(ofSize: 16)
-        cell.detailTextLabel!.text = "Service"
-        cell.detailTextLabel!.font = UIFont.systemFont(ofSize: 12)
-        return cell
+    //cancelボタン押す
+    @IBAction func cancel(_ sender: Any) {
     }
+    
+    
+    
+    
 }
